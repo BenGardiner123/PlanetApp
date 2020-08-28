cbfa3d54ffb2e05f19025c940b399b012521741c
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-08-28 12:25:32 +1000

    fuck this shit

--- planets-app/src/app/app.component.ts
+++ planets-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { PlanetsService } from './planets.service';
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'planets-app';
   public planetsService = PlanetsService;
